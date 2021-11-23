7f18fe90e5dad94cbc86d772ee950676a8a30745
Hedier HAO. Alvarez Ovalle <Reideh@hdv.local>
2021-11-23 14:03:42 -0500

    Version de prueba

--- Mi_Primer_App/app/src/main/java/com/example/mi_primer_app/ui/home/HomeViewModel.kt
+++ Mi_Primer_App/app/src/main/java/com/example/mi_primer_app/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Esta es la vista del INICIO"
     }
     val text: LiveData<String> = _text
 }
