6d20dfcace3d212f4e7a189bf9c178e557287ee6
Reideh17 <hedier.alvarez@gmail.com>
2021-11-23 22:37:45 -0500

    reto1

---
+++ reto_uno/app/src/main/java/com/example/reto_uno/splash.java
@@ -0,0 +1,32 @@
+package com.example.reto_uno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+
+    private final int Time = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },Time);
+    }
+}
