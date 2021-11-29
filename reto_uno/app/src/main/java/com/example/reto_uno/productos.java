b509c972228e3c874a5bb2cd65cdcd2bf0b050d0
Reideh17 <hedier.alvarez@gmail.com>
2021-11-28 20:24:51 -0500

    Productos

--- reto_uno/app/src/main/java/com/example/reto_uno/productos.java
+++ reto_uno/app/src/main/java/com/example/reto_uno/productos.java
@@ -3,18 +3,26 @@ package com.example.reto_uno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class productos extends AppCompatActivity {
 
-    private EditText codigo = (EditText)findViewById(R.id.textView);
+    private EditText codigo;// = (EditText)findViewById(R.id.editTextNumber);
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+        codigo =  (EditText)findViewById(R.id.editTextNumber);
+        resultado = (TextView)findViewById((R.id.textView2)) ;
     }
-    public void agregar{
+    public void agregar(View view){
+        int cod_producto = Integer.parseInt(codigo.getText().toString());
+
+        resultado.setText(cod_producto);
 
     }
 }
